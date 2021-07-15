d911b108f47854872972c4b76fc72b041c2a44a9
Alm4my <thenixadm@gmail.com>
2021-07-15 02:07:07 +0000

    Refactor code

--- src/main/java/fr/almamy/fileupload/controller/UploadController.java
+++ src/main/java/fr/almamy/fileupload/controller/UploadController.java
@@ -1,7 +1,6 @@
 package fr.almamy.fileupload.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +11,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@PropertySource("classpath:config/upload.properties")
 public class UploadController {
     // == Constants ==
-    @Value("${file.upload.max.size: 300}")
-    public int MAX_UPLOAD_SIZE;
     public String FILE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\uploads\\";
 
     // == Fields ==
