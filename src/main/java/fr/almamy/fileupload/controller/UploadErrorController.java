f1880fed78a155748cb5239ec496b2f92d2007cc
Alm4my <thenixadm@gmail.com>
2021-07-15 01:29:54 +0000

    Implement show of different error pages according to the error type

--- src/main/java/fr/almamy/fileupload/controller/UploadErrorController.java
+++ src/main/java/fr/almamy/fileupload/controller/UploadErrorController.java
@@ -4,15 +4,31 @@ import fr.almamy.util.Mappings;
 import fr.almamy.util.ViewNames;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
 public class UploadErrorController implements ErrorController {
     @RequestMapping(Mappings.ERROR)
-    public String error(){
+    public String error(HttpServletRequest request){
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         log.info("Error Occurred");
+
+        if (status != null){
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value())
+                return ViewNames.ERROR_404;
+
+            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+                return ViewNames.ERROR_500;
+
+            }
         return ViewNames.ERROR;
     }
 }
--- src/main/java/fr/almamy/util/ViewNames.java
+++ src/main/java/fr/almamy/util/ViewNames.java
@@ -3,6 +3,8 @@ package fr.almamy.util;
 public final class ViewNames {
     // == Constants ==
     public static final String ERROR = "error";
+    public static final String ERROR_404 = "error/404-error";
+    public static final String ERROR_500 = "error/500-error";
 
     // == Constructors ==
     private ViewNames(){}
