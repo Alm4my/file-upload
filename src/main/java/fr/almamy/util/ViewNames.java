d1ceb2a995789488ff3f95e06100296a89fa7dce
Alm4my <thenixadm@gmail.com>
2021-07-15 01:40:11 +0000

    Create a default error page

--- src/main/java/fr/almamy/util/ViewNames.java
+++ src/main/java/fr/almamy/util/ViewNames.java
@@ -2,7 +2,7 @@ package fr.almamy.util;
 
 public final class ViewNames {
     // == Constants ==
-    public static final String ERROR = "error";
+    public static final String ERROR = "error/error";
     public static final String ERROR_404 = "error/404-error";
     public static final String ERROR_500 = "error/500-error";
 
