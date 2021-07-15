cbc40b3f2cfadc63932ea12c707df235625bf835
Alm4my <thenixadm@gmail.com>
2021-07-15 02:05:47 +0000

    Get value from property file

--- src/main/java/fr/almamy/fileupload/controller/UploadController.java
+++ src/main/java/fr/almamy/fileupload/controller/UploadController.java
@@ -1,17 +1,24 @@
 package fr.almamy.fileupload.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@PropertySource("classpath:config/upload.properties")
 public class UploadController {
+    // == Constants ==
+    @Value("${file.upload.max.size: 300}")
+    public int MAX_UPLOAD_SIZE;
+    public String FILE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\uploads\\";
+
     // == Fields ==
     Map<String, Object> result = new HashMap<>();
 
@@ -23,10 +30,8 @@ public class UploadController {
         System.out.println("File type = " + file.getContentType());
 
         // Save to disk
-        // file path example 1) Windows c:/, 2) Mac ~/Documents/
-        String filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\uploads\\";
         try{
-            file.transferTo(new File(filePath + file.getOriginalFilename()));
+            file.transferTo(new File(FILE_PATH + file.getOriginalFilename()));
             result.put("Success", true);
         } catch (Exception e){
             result.put("Success", false);
